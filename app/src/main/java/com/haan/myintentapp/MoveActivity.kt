e92f00ebf4d35d6c1dbe6a34d933d96ad47e8ead
hannnnzz <adhystirar@gmail.com>
2020-10-04 12:52:38 +0700

    first commit

---
+++ app/src/main/java/com/haan/myintentapp/MoveActivity.kt
@@ -0,0 +1,14 @@
+package com.haan.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+
+class MoveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
+
